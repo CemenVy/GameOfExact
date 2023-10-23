ac1780536d24368c4fd7f3122080f7e662467a71
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-23 11:59:20 +0400

    Initial Commit

---
+++ GameOfExact/GameOfExactApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfExactApp.swift
+//  GameOfExact
+//
+//  Created by Семен Выдрин on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfExactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
